7cd7f1e758f07de84c3add1cde1692742f688ca4
pandurx <internalfire5@live.com>
2018-04-18 16:04:16 -0400

    update

---
+++ search-portal/src/Search.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+ 
+class Search extends Component {
+  render() {
+    return (
+      <div>
+
+        
+        <h2>HELLO</h2>
+
+        <div>
+          <form>
+          
+            <input type="text"/>
+            <input type="submit"/>
+
+          </form>
+        </div>
+      
+
+
+      </div>
+    );
+  }
+}
+ 
+export default Search;
